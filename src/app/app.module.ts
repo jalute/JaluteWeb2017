499fee4f4ba4ee0ea2e256bb0e9db845bdc0f78d
Jalute Mark George <mark.j.george@outlook.com>
2017-07-19 22:25:48 -0400

    Intialial build (unfinished)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResumeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'resume', component: ResumeComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'}
+    ], {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //selector: 'pm-content',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
+})
+export class HomeComponent {
+  pageTitle: string = 'Home Page Title';
+}
